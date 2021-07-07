6a2a1fc70fba8ae9a18d829b8ff42f82dad28d08
Antonio_Aranda7 <L17390318@chetumal.tecnm.mx>
2021-07-06 22:18:01 -0500

    funcional

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'AY90'`, () => {
+  it(`should have as title 'formGroupAnidado'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AY90');
+    expect(app.title).toEqual('formGroupAnidado');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('AY90 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('formGroupAnidado app is running!');
   });
 });
